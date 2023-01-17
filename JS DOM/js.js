735d3ade87f2ad969cbabcc94de4e99e1f102d6c
broharigunda <brohari740@gmail.com>
2023-01-17 16:46:49 +0530

    js dom learning update

--- JS DOM/js.js
+++ JS DOM/js.js
@@ -2,5 +2,10 @@ const ul = document.querySelector('ul');
 const li =  document.createElement('li');
 ul.append(li);
 li.innerText="hello";
-console.log(ul);
-li.setAttribute("class","text")
+console.log(ul.parentNode);
+console.log(ul.parentElement);
+console.log(ul.parentNode);
+console.log(ul.childNodes);
+console.log(ul.children);
+
+li.classList.add('text');
