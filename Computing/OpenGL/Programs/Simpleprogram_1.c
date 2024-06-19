91ed4dcc5218206d64948a9c98d37f33aca96479
Cibi-P <ci230801053@e-consystems.com>
2024-06-19 06:26:47 +0530

    OpenGL

---
+++ Computing/OpenGL/Programs/Simpleprogram_1.c
@@ -0,0 +1,54 @@
+#include <glad/glad.h> // It should be included first to avoid multiple times included error
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+#define true 1
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+    glfwInit();
+    // This tell the OpenGL version 3.3, this ensure if user dont have the proper OpenGL version, it fails
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Telling the GLFW that we want Core-Profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        //std::cout << "Failed to create GLFW window" << std::endl;
+        printf("Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        //std::cout << "Failed to initialize GLAD" << std::endl;
+        printf("Failed to initialize GLAD\n");
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);  
+
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }  
+    return 0;
+}
