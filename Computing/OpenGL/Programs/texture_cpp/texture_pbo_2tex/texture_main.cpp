fc2b501ea21c58c1fa098fc89aa1130a16489a19
Cibi-P <ci230801053@e-consystems.com>
2024-07-24 19:46:32 +0530

    updated PBO

---
+++ Computing/OpenGL/Programs/texture_cpp/texture_pbo_2tex/texture_main.cpp
@@ -0,0 +1,288 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "../../stb_image.h"
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#include "learnopengl/shader_s.h"
+
+
+#define TEX_RESOURCE_FREE 0
+#define TEX_RESOURCE_USED 1
+#define PBO_USE
+GLuint pbo;
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+GLFWwindow* window;
+int width, height, nrChannels,color = 0;
+unsigned int tex0, tex1, texture;
+unsigned int VBO, VAO, EBO;
+unsigned char* data;
+
+#define MAX_PARALLEL_TEX 1
+
+unsigned char texStatusArr[MAX_PARALLEL_TEX] = {0};
+
+unsigned int ProgramObject;
+GLuint texture1;
+
+sem_t mutex, statusUpdateMutex;
+
+int updateFrame();
+void* thread1_fun(void* arg);
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // build and compile our shader zprogram
+    // ------------------------------------
+    Shader ourShader("vshader.vs", "fshader.fs"); 
+    ProgramObject = ourShader.ID;
+    texture1 = glGetUniformLocation(ourShader.ID, "texture1");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        // positions          // colors           // texture coords
+         1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+    };
+    unsigned int indices[] = {  
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    stbi_set_flip_vertically_on_load(true);  
+    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+    data = stbi_load("image2x4k.png", &width, &height, &nrChannels, 0);
+    if (!data)
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    // PBO part
+#ifdef PBO_USE
+    glGenBuffers(1, &pbo);
+    glBindBuffer( GL_PIXEL_UNPACK_BUFFER, pbo );
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, width*height*4, 0, GL_STREAM_DRAW);
+#endif
+
+    // load and create a texture 
+    // -------------------------
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glGenTextures(1, &tex0);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex0); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+glGenTextures(1, &tex1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, tex1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    // render loop
+    // -----------
+    sem_init( &mutex, 1, MAX_PARALLEL_TEX);
+    sem_init( &statusUpdateMutex, 1, 1);
+    pthread_t thread1, thread2;
+    pthread_create(&thread1, NULL, thread1_fun, NULL );
+   // pthread_join(thread1, NULL);
+    while(!glfwWindowShouldClose(window)){
+        updateFrame();
+    }
+    stbi_image_free(data);
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+void* thread1_fun(void* arg)
+{
+    //printf("Afdafaf");
+
+    while (!glfwWindowShouldClose(window))
+    //while (1)
+    {
+        updateFrame();
+    }
+}
+
+int updateFrame()
+{
+    int freeTex = 0;
+    sem_wait(&mutex);
+    sem_wait(&statusUpdateMutex);
+    for( freeTex = 0; freeTex < MAX_PARALLEL_TEX; freeTex++ )
+    {
+        if( texStatusArr[freeTex] == TEX_RESOURCE_FREE )
+            break;
+    }
+
+    texStatusArr[freeTex] = TEX_RESOURCE_USED;
+    sem_post(&statusUpdateMutex);
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // bind Texture
+        if( freeTex == 0 )
+        {
+        glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, tex0);
+#ifdef PBO_USE
+            glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+#endif
+        }
+        else if( freeTex == 1 )
+        {
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, tex1);
+        }
+        else{
+            return 0;
+        }
+        memset( data, color, width*height*4);
+        color++;
+        if(color >254)
+            color = 0;
+
+
+#ifdef PBO_USE
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER,width*height*4, 0, pbo);
+    GLubyte* ptr = (GLubyte*) glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
+    if(ptr)
+    {
+        memset( ptr, 128, width*height*4);
+        glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+    }
+#else
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
+#endif
+
+        // render container
+        //ourShader.use();
+        glUseProgram(ProgramObject);
+        if( freeTex == 0 )
+        glUniform1i( texture1, 0);
+        else if(freeTex == 1)
+            glUniform1i(texture1, 1);
+        else
+            return 0;
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        std::cout<<"currTex: "<<freeTex<<" tex1:"<<(int) texStatusArr[0]<<" tex2:"<<(int) texStatusArr[1]<<std::endl;
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    sem_wait(&statusUpdateMutex);
+    texStatusArr[freeTex] = TEX_RESOURCE_FREE;
+    sem_post(&statusUpdateMutex);
+    sem_post(&mutex);
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
