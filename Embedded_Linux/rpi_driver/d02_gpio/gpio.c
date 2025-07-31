812249e18089aa034084ab275c6c09cb93a57a50
cibi-p <brohari740@gmail.com>
2025-07-31 21:50:29 +0530

    GPIO driver Added

--- Embedded_Linux/rpi_driver/d02_gpio/gpio.c
+++ Embedded_Linux/rpi_driver/d02_gpio/gpio.c
@@ -1,15 +1,50 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/gpio/consumer.h>
+
+static struct gpio_desc *led, *button;
+
+#define IO_LED 21
+#define IO_BUTTON 20
+
+#define IO_OFFSET 512
 
 int __init my_init(void)
 {
+  int status;
 	printk("hello - Hello, Kernel!\n");
+
+  led = gpio_to_desc(IO_LED + IO_OFFSET);
+  if (!led) {
+      printk("no gpio detected1");
+      return -ENODEV;
+  }
+  button = gpio_to_desc(IO_BUTTON + IO_OFFSET);
+  if (!button) {
+      printk("no gpio detected");
+      return -ENODEV;
+  }
+
+  status = gpiod_direction_output(led, 0);
+  if (status) {
+      printk("gpio direction error");
+      return -ENODEV;
+  }
+  
+  status = gpiod_direction_input(button);
+  if (status) {
+      printk("gpio direction error");
+      return -ENODEV;
+  }
+
+  gpiod_set_value(led, 1);
 	return 0;
 }
 
 void __exit my_exit(void)
 {
 	printk("hello - Goodbye, Kernel!\n");
+  gpiod_set_value(led, 0);
 }
 
 module_init(my_init);
