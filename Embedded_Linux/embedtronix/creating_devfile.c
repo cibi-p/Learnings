6d79cb69acc8b3eeca5c329d264f68854baa31a2
cibi-p <brohari740@gmail.com>
2025-10-08 21:56:30 +0530

    device fops works

--- Embedded_Linux/embedtronix/creating_devfile.c
+++ Embedded_Linux/embedtronix/creating_devfile.c
@@ -14,6 +14,7 @@
 #include<linux/device.h>
 #include<linux/fs.h>
 #include<linux/err.h>
+#include<linux/cdev.h>
  
 
 #if 0
@@ -50,6 +51,59 @@
 
 dev_t dev = 0;
 static struct class *dev_class;
+static struct cdev etx_cdev;
+/*
+** Function Prototypes
+*/
+static int      __init hello_world_init(void);
+static void     __exit hello_world_exit(void);
+static int      etx_open(struct inode *inode, struct file *file);
+static int      etx_release(struct inode *inode, struct file *file);
+static ssize_t  etx_read(struct file *filp, char __user *buf, size_t len,loff_t * off);
+static ssize_t  etx_write(struct file *filp, const char *buf, size_t len, loff_t * off);
+
+static struct file_operations fops =
+{
+    .owner      = THIS_MODULE,
+    .read       = etx_read,
+    .write      = etx_write,
+    .open       = etx_open,
+    .release    = etx_release,
+};
+/*
+** This function will be called when we open the Device file
+*/
+static int etx_open(struct inode *inode, struct file *file)
+{
+        pr_info("Driver Open Function Called...!!!\n");
+        return 0;
+}
+/*
+** This function will be called when we close the Device file
+*/
+static int etx_release(struct inode *inode, struct file *file)
+{
+        pr_info("Driver Release Function Called...!!!\n");
+        return 0;
+}
+/*
+** This function will be called when we read the Device file
+*/
+static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("Driver Read Function Called...!!!\n");
+        return 0;
+}
+/*
+** This function will be called when we write the Device file
+*/
+static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("Driver Write Function Called...!!!\n");
+        return len;
+}
+
+
 /*
 ** Module init function
 */
@@ -61,6 +115,14 @@ static int __init hello_world_init(void)
                 return -1;
         };
         pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+        /*Creating cdev structure*/
+        cdev_init(&etx_cdev,&fops);
+        /*Adding character device to the system*/
+        if((cdev_add(&etx_cdev,dev,1)) < 0){
+            pr_err("Cannot add the device to the system\n");
+            goto r_class;
+        }
+
         /*Creating struct class*/
         dev_class = class_create("test_class");
         if(IS_ERR(dev_class)){
@@ -101,6 +163,7 @@ static void __exit hello_world_exit(void)
 {
     device_destroy(dev_class,dev);
     class_destroy(dev_class);
+    cdev_del(&etx_cdev);
     unregister_chrdev_region(dev, 1);
     printk(KERN_INFO "Kernel Module Removed Successfully...\n");
 }
